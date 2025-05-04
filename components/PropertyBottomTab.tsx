e92515f2931c480bc45c68b77cb17c1a51c4bc6a
lky688 <alen56560@gmail.com>
2025-05-05 00:27:23 +0800

    Update PropertyBottomTab.tsx

--- components/PropertyBottomTab.tsx
+++ components/PropertyBottomTab.tsx
@@ -1,9 +1,10 @@
 import { View, Text, StyleSheet, Pressable } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type Props = {
     propertyData: any,
@@ -14,6 +15,43 @@ const PropertyBottomTab = ({propertyData, addToWishlist}: Props) => {
     const navigation = useNavigation();
     const [liked, setLiked] = useState(false);
 
+    const fetchWishlistItemFromDB = () => {
+        AsyncStorage.getItem('currentUser')
+          .then((userString) => {
+            if (userString) {
+              const user = JSON.parse(userString);
+              const userID = parseInt(user.id);
+              const propertyID = String(propertyData.id);
+
+              fetch(`http://10.0.2.2:5000/api/wishlist/user/${userID}/property/${propertyID}`)
+                .then((response) => {
+                  return response.json();
+                })
+                .then((data) => {
+                  console.log('Fetched wishlist item from database:', data);
+                  if (Object.keys(data).length !== 0) {
+                    // Current property is saved as wishlist in the database
+                    setLiked(true);
+                  }
+                })
+                .catch((error) => {
+                  console.error('Error fetching wishlist item:', error);
+                })
+            } else {
+              console.log('No user found in AsyncStorage');
+            }
+          })
+          .catch((error) => {
+            console.error('Error reading AsyncStorage:', error);
+          });
+      };
+      
+      
+      
+    useEffect(() => {
+        fetchWishlistItemFromDB()
+        }, []);
+
     const handleReserve = () => {
         navigation.navigate('BookingScreen', {property: propertyData});
     };
@@ -23,7 +61,62 @@ const PropertyBottomTab = ({propertyData, addToWishlist}: Props) => {
 
         if(!liked) {
             addToWishlist(propertyData);
+            saveWishlistToDB();
+        } else {
+            removeWishlistFromDB();
         }
+    };
+      
+    const saveWishlistToDB = () => {
+      
+        AsyncStorage.getItem('currentUser')
+        .then((userString) => {
+          if (userString) {
+            const user = JSON.parse(userString);
+            const userID = user.id;
+  
+            fetch('http://10.0.2.2:5000/api/wishlist', {  // Replace with your LAN IP!
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify({userID: userID, propertyID: propertyData.id}),
+            })
+              .then((response) => response.json())
+              .then((data) => {
+                console.log('Wishlist added to database:', data);
+              })
+              .catch((error) => {
+                console.error('Error:', error);
+              })
+        }})
+        .catch((error) => {
+          console.error('Error reading AsyncStorage:', error);
+        });
+    };
+
+      const removeWishlistFromDB = () => {
+      
+        AsyncStorage.getItem('currentUser')
+        .then((userString) => {
+          if (userString) {
+            const user = JSON.parse(userString);
+            const userID = user.id;
+  
+            fetch('http://10.0.2.2:5000/api/wishlist', {  // Replace with your LAN IP!
+              method: 'DELETE',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify({userID: userID, propertyID: propertyData.id}),
+            })
+              .then((response) => response.json())
+              .then((data) => {
+                console.log('Wishlist removed from database:', data);
+              })
+              .catch((error) => {
+                console.error('Error:', error);
+              })
+        }})
+        .catch((error) => {
+          console.error('Error reading AsyncStorage:', error);
+        });
     };
 
     return (
