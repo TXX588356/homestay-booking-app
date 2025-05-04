30edc5617f12c7f239bc99dfacae0a29bc482bf8
lky688 <alen56560@gmail.com>
2025-05-05 00:24:05 +0800

    Update TripDetails.tsx

--- Tab/TripStack/TripDetails.tsx
+++ Tab/TripStack/TripDetails.tsx
@@ -1,12 +1,50 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { View, Text } from 'react-native'
+import { ExternalStyles } from '../../Styles';
+import BackButton from '../../components/BackButton';
 
 const TripDetails = ({route, navigation}: any) => {
+
+  const {booking, property} = route.params;
+  const serviceFees = booking.numDays * property.price * 0.10;
+  const totalPrice = (booking.numDays * property.price) + serviceFees;
+  const paymentMethod = booking.paymentMethod === 'card' ? "Credit Card" : "FPX Online Banking";
+
   return (
     <View>
-      <Text>Trip Details</Text>
+      <View style={ExternalStyles.backButtonContainer}>
+        <BackButton/>
+      </View>
+
+      <View style={[ExternalStyles.ContainerWithUnderline, {backgroundColor: 'lightgrey', padding: 10, marginTop: 80}]}>
+        <Text style={ExternalStyles.sectionTitle}>Booking Details</Text>
+        <Text style={{marginVertical: 5}}>{property.name}</Text>
+        <Text style={{marginVertical: 5}}>
+          {booking.startDate} ~ {booking.endDate} ({booking.numDays} nights)
+        </Text>
+        <Text style={{marginVertical: 5}}>Guests: {booking.numDays} {'\n\n'}</Text>
+      
+        <Text style={ExternalStyles.sectionTitle}>Price Breakdown</Text>
+        <View style={ExternalStyles.priceRow}>
+          <Text>RM{property.price} x {booking.numDays} nights</Text>
+          <Text>RM{(property.price * booking.numDays).toFixed(2)}</Text>
+        </View>
+        <View style={ExternalStyles.priceRow}>
+          <Text>Service Fee (10%)</Text>
+          <Text>RM{(serviceFees).toFixed(2)}</Text>
+        </View>
+        <View style={ExternalStyles.totalRow}>
+          <Text style={ExternalStyles.sectionTitle}>Total</Text>
+          <Text style={ExternalStyles.sectionTitle}>RM{totalPrice.toFixed(2)}</Text>
+        </View>
+      
+      </View>
+      <View>
+          <Text style={ExternalStyles.sectionTitle}>Payment Method</Text>
+          <Text>{paymentMethod}</Text>
+      </View>
     </View>
-  )
+  );
 }
 
 export default TripDetails;
