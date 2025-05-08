f90ffebcd9fad727e4ea8dfeef0bf7ebcb86dac4
lky688 <alen56560@gmail.com>
2025-05-08 23:32:34 +0800

    added new paths

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 const config = {
+    // Change the IPs accordingly
     settings: {
-      serverPath: 'http://10.0.2.2:5000' // Change this accordingly
+      livechatServerPath: 'http://10.0.2.2:5000', // used in websocket for live chat
+      bookingServerPath: 'http://10.0.2.2:5001', // used in interacting with database for bookings via fetch API
+      wishlistServerPath: 'http://10.0.2.2:5002', // used in interacting with database for wishlist via fetch API
     }
   };
   
