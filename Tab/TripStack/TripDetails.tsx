96fcd3dd287b2707ac0acdbdbe0eb30526318583
lky688 <alen56560@gmail.com>
2025-05-10 07:56:49 +0800

    add open map feature

--- Tab/TripStack/TripDetails.tsx
+++ Tab/TripStack/TripDetails.tsx
@@ -1,9 +1,11 @@
 import React, { useContext } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Linking, ScrollView } from 'react-native'
 import { ExternalStyles } from '../../Styles';
 import BackButton from '../../components/BackButton';
 import { ThemeContext } from '../../util/ThemeManager';
 import ThemedText from '../../components/ThemedText';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { createMapLink } from 'react-native-open-maps';
 
 const TripDetails = ({route, navigation}: any) => {
   const { theme } = useContext(ThemeContext);
@@ -13,13 +15,18 @@ const TripDetails = ({route, navigation}: any) => {
   const totalPrice = (booking.numDays * property.price) + serviceFees;
   const paymentMethod = booking.paymentMethod === 'card' ? "Credit Card" : "FPX Online Banking";
 
+  const openMap = (propertyName: string) => { 
+          const mapLink = createMapLink({ provider: 'google', query: propertyName });
+          Linking.openURL(mapLink);
+      }
+
   return (
-    <View style={{backgroundColor: theme.background, flex: 1}}>
+    <ScrollView style={{backgroundColor: theme.background, flex: 1}}>
       <View style={ExternalStyles.backButtonContainer}>
         <BackButton/>
       </View>
 
-      <View style={[ExternalStyles.ContainerWithUnderline, {backgroundColor: 'lightgrey', padding: 10, marginTop: 80}]}>
+      <View style={[ExternalStyles.ContainerWithUnderline, {backgroundColor: 'lightgrey', padding: 10, marginTop: 60}]}>
         <Text style={ExternalStyles.sectionTitle}>Booking Details</Text>
         <Text style={{marginVertical: 5}}>{property.name}</Text>
         <Text style={{marginVertical: 5}}>
@@ -42,11 +49,24 @@ const TripDetails = ({route, navigation}: any) => {
         </View>
       
       </View>
-      <View style={{margin: 10, paddingLeft: 10}}>
+      
+      <View style={ExternalStyles.ContainerWithUnderline}>
           <ThemedText style={ExternalStyles.sectionTitle}>Payment Method</ThemedText>
-          <ThemedText>{paymentMethod}</ThemedText>
+          <ThemedText style={{fontFamily: 'Montserrat-Medium'}}>{paymentMethod}</ThemedText>
       </View>
-    </View>
+
+      <View style={ExternalStyles.ContainerWithUnderline}>
+          <ThemedText style={ExternalStyles.sectionTitle}>Location Address</ThemedText>
+          <ThemedText style={{fontFamily: 'Montserrat-Medium'}}>{property.address}</ThemedText>
+            <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center',}}>
+                <TouchableOpacity style={[ExternalStyles.button, {width: '65%', flexDirection: 'row', justifyContent: 'center'}]} 
+                    onPress={() => openMap(property.name)}>
+                    <Ionicons name="location-outline" size={22} color="white" />
+                      <Text style={[ExternalStyles.buttonText, {marginHorizontal: 8}]}>View in Map</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    </ScrollView>
   );
 }
 
