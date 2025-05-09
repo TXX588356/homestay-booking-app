c04f51c9d31041149c5f70d5d3b14c03afd9ed04
lky688 <alen56560@gmail.com>
2025-05-10 03:08:37 +0800

    add open location in map feature

--- Tab/HomeStack/PropertyDetailsScreen.tsx
+++ Tab/HomeStack/PropertyDetailsScreen.tsx
@@ -1,28 +1,37 @@
-import { View, Dimensions} from 'react-native'
-import React, { useContext } from 'react'
+import { View, Text, Image, Button, Dimensions, StyleSheet, TouchableOpacity, Linking } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParamList } from '../../Types';
 import { ExternalStyles } from '../../Styles';
 export type Props = StackScreenProps<RootStackParamList, 'PropertyDetails'>;
 import CustomCarousel from '../../components/CustomCarousel';
 import { ScrollView } from 'react-native-gesture-handler';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import PropertyBottomTab from '../../components/PropertyBottomTab';
+import Calendar from '../../components/Calendar';
 import LoadingIndicator, { useTransitionLoading } from '../../components/LoadingIndicator';
 import BackButton from '../../components/BackButton';
 import { ThemeContext } from '../../util/ThemeManager';
 import ThemedText from '../../components/ThemedText';
 import { imageMap } from '../../imageMap';
+import { createMapLink } from 'react-native-open-maps';
 
 const { width } = Dimensions.get('window');
 
 const PropertyDetailsScreen = ({route, navigation}: Props) => {
+
     const {data} = route.params;
 
     const loading = useTransitionLoading(navigation);
 
     const { theme } = useContext(ThemeContext);
-         
-    let images = data.images.map(img => imageMap[img])
+        
+    let images = data.images.map(img => imageMap[img]);
+
+    const openMap = (propertyName: string) => { 
+        const mapLink = createMapLink({ provider: 'google', query: propertyName });
+        Linking.openURL(mapLink);
+    }
 
     if(loading) {
         return <LoadingIndicator/>;
@@ -36,7 +45,7 @@ const PropertyDetailsScreen = ({route, navigation}: Props) => {
             </View>
                 <CustomCarousel data={images}/>
 
-                <View style={[ExternalStyles.propertyTitle, {paddingTop: 70}]}>
+                <View style={[ExternalStyles.propertyTitle, {paddingTop: 88}]}>
                     <ThemedText style={ExternalStyles.titleText}>{data.name}</ThemedText>
                     <ThemedText style={ExternalStyles.subTitle}>{data.category} stay in {data.location}</ThemedText>
                 </View>
@@ -53,15 +62,26 @@ const PropertyDetailsScreen = ({route, navigation}: Props) => {
                         <ThemedText key={index} style={{fontSize: 16, marginVertical: 2, lineHeight: 25, fontFamily: 'Montserrat-Medium'}}>- {amenity}</ThemedText>
                     ))}
                 </View>
+                
+                <View style={ExternalStyles.ContainerWithUnderline}>
+                    <ThemedText style={ExternalStyles.sectionTitle}>This place is located at</ThemedText>
+                    <ThemedText style={{fontFamily: 'Montserrat-Medium', marginTop: 3}}>{data.address}</ThemedText>
+                    <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center',}}>
+                        <TouchableOpacity style={[ExternalStyles.button, {width: '65%', flexDirection: 'row', justifyContent: 'center'}]} 
+                            onPress={() => openMap(data.name)}>
+                            <Ionicons name="location-outline" size={22} color="white" />
+                            <Text style={[ExternalStyles.buttonText, {marginHorizontal: 8}]}>View in Map</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
             </ScrollView>
-            
+
             <PropertyBottomTab
                 propertyData={data}
                 addToWishlist={(item) => {
                     console.log("Added to wishlist: ", item)
                 }}/>
         </View>
-    
     )
 }
 
