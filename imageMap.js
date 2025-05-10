dddbbe543a1e574657ba761fc223d6040077f3aa
TX <leetianxin04@gmail.com>
2025-05-10 17:59:46 +0800

    added more data

--- imageMap.js
+++ imageMap.js
@@ -6,6 +6,15 @@ const beach2a = require('./img/beach2a.jpg');
 const beach2b = require('./img/beach2b.jpg');
 const beach2c = require('./img/beach2c.jpg');
 
+const beach3a = require('./img/beach3a.jpg');
+const beach3b = require('./img/beach3b.jpg');
+const beach3c = require('./img/beach3c.jpg');
+
+const beach4a = require('./img/beach4a.jpg');
+const beach4b = require('./img/beach4b.jpg');
+const beach4c = require('./img/beach4c.jpg');
+
+
 
 const cabin1a = require('./img/cabin1a.jpg');
 const cabin1b = require('./img/cabin1b.jpg');
@@ -22,6 +31,10 @@ const mountain3c = require('./img/mountain3c.jpg');
 const villa1a = require('./img/villa1a.jpg');
 const villa1b = require('./img/villa1b.jpg');
 const villa1c = require('./img/villa1c.jpg');
+
+const city2a = require('./img/city2a.jpg');
+const city2b = require('./img/city2b.jpg');
+const city2c = require('./img/city2c.jpg');
 
 const camping1a = require('./img/camping1a.jpg');
 const camping1b = require('./img/camping1b.jpg');
@@ -44,6 +57,14 @@ export const imageMap = {
   './img/beach2b.jpg': beach2b,
   './img/beach2c.jpg': beach2c,
 
+  './img/beach3a.jpg': beach3a,
+  './img/beach3b.jpg': beach3b,
+  './img/beach3c.jpg': beach3c,
+
+  './img/beach4a.jpg': beach4a,
+  './img/beach4b.jpg': beach4b,
+  './img/beach4c.jpg': beach4c,
+
   './img/cabin1a.jpg': cabin1a,
   './img/cabin1b.jpg': cabin1b,
   './img/cabin1c.jpg': cabin1c,
@@ -59,6 +80,10 @@ export const imageMap = {
   "./img/villa1a.jpg": villa1a,
   "./img/villa1b.jpg": villa1b,
   "./img/villa1c.jpg": villa1c,
+
+  "./img/city2a.jpg": city2a,
+  "./img/city2b.jpg": city2b,
+  "./img/city2c.jpg": city2c,
 
   "./img/camping1a.jpg": camping1a,
   "./img/camping1b.jpg": camping1b,
